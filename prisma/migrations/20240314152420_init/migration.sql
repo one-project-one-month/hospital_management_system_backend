11daf22f2601e762debbca57cde23834bcac7784
Kaung Min Htet <kgminhtet.dev@gmail.com>
2024-03-17 07:46:45 +0630

    Changed prisma model field name to camelCase and deleted previous database migrations

--- prisma/migrations/20240314152420_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Room" (
-    "id" SERIAL NOT NULL,
-    "title" VARCHAR(255) NOT NULL,
-
-    CONSTRAINT "Room_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20240316145629_disease_model/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Disease" (
-    "id" SERIAL NOT NULL,
-    "name" VARCHAR(255) NOT NULL,
-
-    CONSTRAINT "Disease_pkey" PRIMARY KEY ("id")
-);
